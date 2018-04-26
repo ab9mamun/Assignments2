ca2669b0f28493dee13497a8d5ec75467cc299f1
Abdullah Mamun <ab9mamun@gmail.com>
2018-04-26 23:16:10 +0600

    dataset created

--- ML_Offline_1/Adaboost/ada.py
+++ ML_Offline_1/Adaboost/ada.py
@@ -1,16 +1,71 @@
 import csv
-def init():
-    with open('bank-additional-full.csv', newline='') as csvfile:
-        bankreader =  csv.reader(csvfile, delimiter=' ', quotechar='|')
-        i = 0
-        for row in bankreader:
-            if(i==3500):
-                print(', '.join(row))
-            i+=1
+import math
+import random
 
+def br():
+    print('=================================================')
+
+def decision_stump():
+    pass
+
+def init():
+    #declare globals
+    global dataset
+    global rowlength
+    global datasize
+    global header
+    #initialize random
+    random.seed(1224)
+
+    #read data file
+    dataset = []
+    br()
+    print('Reading data file...')
+    with open('bank-additional-full.csv', newline='') as csvfile:
+        bankreader =  csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in bankreader:
+            dataset.append(row)
+    csvfile.close()
+
+    #process dataset
+    print('Processing datapoints...')
+    n = len(dataset)
+    rowlength = len(dataset[0])
+    #print(n, 'datapoints found')
+    ydata = []
+    ndata = []
+    for row in dataset:
+        for i  in range(rowlength):
+            entry = row[i]
+            if entry[0] == '"':
+                row[i] = entry[1:len(entry)-1]
+            else:
+                row[i] = float(entry)
+        if row[rowlength-1] == 'no':
+            ndata.append((row[:rowlength-1], 'no'))
+        elif row[rowlength-1] == 'yes':
+            ydata.append((row[:rowlength-1], 'yes'))
+
+    #print(len(ndata), len(ydata))
+    header = dataset[0]
+    dataset = ydata[:]
+
+    #take same numbers of yes and no
+    nlength = len(ndata)
+    ylength = len(ydata)
+
+    for i in range(ylength):
+        j = random.randint(0, nlength-1)
+        dataset.append(ndata[j])
+        ndata.pop(j)
+        nlength -= 1
+
+    datasize = len(dataset)
+    random.shuffle(dataset)
+    print('Dataset created. Number of datapoints:', datasize)
+    br()
 
 def main():
-    print('Starting adaboost program')
     init()
 
 
