b89ead529bd485302d6fc6a5a0cd53e620f82104
Abdullah Mamun <ab9mamun@gmail.com>
2018-04-27 16:19:24 +0600

    Dataset processed again

--- ML_Offline_1/Adaboost/ada.py
+++ ML_Offline_1/Adaboost/ada.py
@@ -2,17 +2,37 @@ import csv
 import math
 import random
 
+train_data = []
+test_data = []
+
 def br():
     print('=================================================')
 
-def decision_stump():
+def yncounter(dataset):
+    y = 0
+    n = 0
+    for row in dataset:
+        if row[1] == 'no':
+            n+=1
+        elif row[1] == 'yes':
+            y+=1
+    return y, n
+
+
+def entropy(train_data, feature_index):
+    pass
+
+def next_attribute(train):
+    pass
+
+def decision_stump(train):
+
     pass
 
 def init():
     #declare globals
-    global dataset
+    global train_data, test_data
     global rowlength
-    global datasize
     global header
     #initialize random
     random.seed(1224)
@@ -48,21 +68,26 @@ def init():
 
     #print(len(ndata), len(ydata))
     header = dataset[0]
-    dataset = ydata[:]
 
     #take same numbers of yes and no
     nlength = len(ndata)
     ylength = len(ydata)
+    sel_ndata = []
 
     for i in range(ylength):
         j = random.randint(0, nlength-1)
-        dataset.append(ndata[j])
+        sel_ndata.append(ndata[j])
         ndata.pop(j)
         nlength -= 1
 
-    datasize = len(dataset)
-    random.shuffle(dataset)
-    print('Dataset created. Number of datapoints:', datasize)
+    train_data = ydata[:int(ylength*2/3)] + sel_ndata[:int(ylength*2/3)]
+    test_data = ydata[int(ylength*2/3):] + sel_ndata[int(ylength * 2 / 3):]
+
+    random.shuffle(train_data)
+    random.shuffle(test_data)
+    print('Dataset created. Number of datapoints:', len(train_data) + len(test_data))
+    print('Train data: ', len(train_data), 'Test data:', len(test_data))
+    print('Yes/No counter:', yncounter(train_data), yncounter(test_data))
     br()
 
 def main():
