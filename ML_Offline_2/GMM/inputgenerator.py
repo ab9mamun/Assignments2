3b49393b43de1cc14873fd845a2f4fa302a92c22
Abdullah Mamun <ab9mamun@gmail.com>
2018-05-05 06:26:32 +0600

    inputgenerator created

---
+++ ML_Offline_2/GMM/inputgenerator.py
@@ -0,0 +1,32 @@
+import numpy
+import numpy.random as rand
+import matplotlib.pyplot as plt
+
+SEED = 34
+rand.seed(SEED)
+k = rand.randint(3, 10)
+total = 300 - 300%k  + k #the next integer divisible by k
+xs = []
+ys = []
+labels = []
+
+for i in range (k):
+    mu = [rand.uniform(-10, 10), rand.uniform(-10, 10)]
+   # print(mu)
+    cov = [ [rand.uniform(0.1, 5/k),0], [0, rand.uniform(0.1, 5/k)] ]
+    #print(cov)
+    x, y = numpy.random.multivariate_normal(mu, cov,total//k ).T
+    xs = numpy.concatenate((xs, x))
+    ys = numpy.concatenate((ys, y))
+    labels = labels+[i]*(total//k)
+
+fig = plt.figure()
+plt.scatter(xs, ys, 20, c=labels)
+fig.savefig("true-map.png")
+
+with open('input.txt', 'w') as file:
+    file.write(str(k)+'\n')
+    file.write(str(total)+'\n')
+    for i in range(total):
+        file.write(str(xs[i])+' ' + str(ys[i])+'\n')
+    file.close()
--- ML_Offline_2/GMM/test.py
+++ ML_Offline_2/GMM/test.py
@@ -1,1 +1,6 @@
+import random
 print('Hello world')
+def randf(low, high):
+    return low + random.random()*(high - low)
+while True:
+    print(randf(-12,56))
