3f4391e07b5fb73e6237c289e4638973edadbd54
Abdullah Mamun <ab9mamun@gmail.com>
2018-05-14 16:23:58 +0600

    Printing neural network is done

--- PatternOffline1/NeuralNet/neural.py
+++ PatternOffline1/NeuralNet/neural.py
@@ -1,9 +1,21 @@
 import numpy
+import numpy.random as rand
+
+#supporitng functions ---
+def br():
+    print('----------------------------------------------')
+
+
 
 class Neuron:
     def __init__(self, w_length):
-        self.weight = [0.1]*w_length
+        self.weight = [rand.uniform(0, 1)]*w_length
         self.delta = 0
+        self.output = 0
+
+    def print(self, header):
+        print(header,  self.weight)
+
 
 class Layer:
     def __init__(self, num_neuron, w_length):  #w_length = num_neuron_prev + 1 for bias
@@ -13,6 +25,13 @@ class Layer:
         for i in range(num_neuron):
             self.neurons[i] = Neuron(w_length)
 
+    def print(self, header):
+        print(header)
+        br()
+        print('Number of neurons: ', self.num_neuron)
+        for j in range(self.num_neuron):
+            self.neurons[j].print('Neuron '+ str(j) + ':')
+
 
 class Network:
     def __init__(self, num_attributes, num_classes, num_Hlayer, num_neurons):
@@ -21,16 +40,25 @@ class Network:
         self.num_attributes = num_attributes
         self.num_classes = num_classes
 
-        self.Hlayers[0] = Layer(num_neurons[0], num_attributes)
+        self.Hlayers[0] = Layer(num_neurons[0], num_attributes+1)
         for i in range(1, num_Hlayer):
-            self.layers[i] = Layer(num_neurons[i], num_neurons[i-1])
+            self.Hlayers[i] = Layer(num_neurons[i], num_neurons[i-1]+1)
 
-        self.outputLayer = Layer(num_classes, num_neurons[num_Hlayer-1])
+        self.outputLayer = Layer(num_classes, num_neurons[num_Hlayer-1]+1)
 
         self.num_Hlayer = num_Hlayer
         self.num_neurons = num_neurons.copy()
 
+    def print(self):
+        br()
+        print("Printing Neural Network")
+        br()
+        print('Number of hidden layers: ', self.num_Hlayer)
+        for i in range(self.num_Hlayer):
+            self.Hlayers[i].print('Hidden layer '+str(i)+":")
 
+        self.outputLayer.print('Output layer:')
+        br()
 
 
     def train(self, data):
@@ -41,10 +69,11 @@ class Network:
 
 
 def main():
-    print('Hello world')
+    rand.seed(1112)
+
     input_ = numpy.loadtxt('input.txt', dtype=int)
     #print(input_)
-    data = numpy.loadtxt('data.txt', dtype=int)
+    dataset = numpy.loadtxt('data.txt', dtype=int)
     #print(data)
     num_attributes = input_[0]
     num_classes = input_[1]
@@ -53,10 +82,14 @@ def main():
     for i in range(num_Hlayer):
         num_neurons[i] = input_[i+3]
 
-    print(num_neurons)
+    print('Number of attributes:', num_attributes)
+    print('Number of classes:', num_classes)
+    print('Number of hidden layers:', num_Hlayer)
+    print('Number of neurons in each hidden layer:', num_neurons)
 
     network = Network(num_attributes, num_classes, num_Hlayer, num_neurons)
-    network.train(data)
+    network.train(dataset)
+    network.print()
 
 
 if __name__ == '__main__':
