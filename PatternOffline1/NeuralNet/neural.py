0e5f670b6e2d0b8b92f1ffe89ee6181c0ac62dff
Abdullah Mamun <ab9mamun@gmail.com>
2018-05-14 13:51:57 +0600

    working with input

--- PatternOffline1/NeuralNet/neural.py
+++ PatternOffline1/NeuralNet/neural.py
@@ -1,3 +1,21 @@
+import numpy;
+
+class Neuron:
+    def __init__(self, w_length, ):
+        self.weight
+        self.delta = 0
+
+
+class Network:
+    def __init__(self):
+        self.neurons = [[]]
+
 def main():
     print('Hello world')
+    input_ = numpy.loadtxt('input.txt', dtype=int)
+    print(input_)
+    data = numpy.loadtxt('data.txt', dtype=int)
+    print(data)
 
+if __name__ == '__main__':
+    main()
