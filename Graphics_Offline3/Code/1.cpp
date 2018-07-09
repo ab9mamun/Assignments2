e87cc803c308e17c83d8e7e1a4dcdd5745697a08
Abdullah Mamun <ab9mamun@gmail.com>
2018-07-09 06:03:03 +0600

    Creating data structures for z buffer

--- Graphics_Offline3/Code/1.cpp
+++ Graphics_Offline3/Code/1.cpp
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#include "bitmap_image.hpp"
+
 #define CAP 300
 #define pi (2*acos(0.0))
 using namespace std;
@@ -22,30 +24,24 @@ public:
         this->x = x; this->y = y; this->z = z;
     }
     static Vector zero(){
-        Vector v(0,0,0);
-        return v;
+        return Vector(0,0,0);
     }
     static Vector X(){
-        Vector v(1,0,0);
-        return v;
+        return Vector(1,0,0);
     }
     static Vector Y(){
-        Vector v(0,1,0);
-        return v;
+        return Vector(0,1,0);
     }
     static Vector Z(){
-        Vector v(0,0,1);
-        return v;
+        return Vector(0,0,1);
     }
 
 
     Vector operator-(){
-        Vector v(-x, -y, -z);
-        return v;
+        return Vector(-x, -y, -z);
     }
     Vector operator+(Vector v){
-        Vector w(x+v.x, y+v.y, z+v.z);
-        return w;
+        return Vector(x+v.x, y+v.y, z+v.z);
     }
     Vector operator-(Vector v){
         return (*this) + (-v);
@@ -57,8 +53,7 @@ public:
         return sqrt(length2());
     }
     Vector operator*(double c){
-        Vector v(x*c, y*c, z*c);
-        return v;
+        return Vector(x*c, y*c, z*c);
     }
 
     Vector operator/(double c){
@@ -74,10 +69,9 @@ public:
     }
 
     Vector operator*(Vector v){
-        Vector w(y * v.z - z * v.y,
+        return Vector(y * v.z - z * v.y,
                  -(x * v.z - z * v.x),
                  x * v.y - y * v.x);
-        return w;
     }
 
     double operator[](int i){
@@ -92,27 +86,6 @@ public:
         n = n.unit();
         Vector a = *this;
         return a - n* 2* a.dot(n);
-    }
-
-    ///rotate respect to a perpendicular axis
-    Vector rotatePA(double angle_deg, Vector axis = Z()){
-        double A = angle_deg*pi/180.0;
-        Vector r = axis.unit();
-        Vector l = (*this);
-        Vector u = r*l;
-        u = l*cos(A) + u*sin(A);
-        return u;
-    }
-
-    Vector perp2d(){
-        Vector v(-y, x, 0);
-        return v;
-    }
-
-    Vector rotate2d(double angle_deg){
-        double A = angle_deg*pi/180.0;
-        Vector u = (*this)*cos(A) + perp2d()*sin(A);
-        return u;
     }
 
     void set(double x, double y, double z){
@@ -140,13 +113,11 @@ public:
 	Vector asVector(){ Vector v(x,y,z); return v;}
 
 	Point operator+(Vector v){
-        Point p(x + v.x, y+v.y, z+v.z);
+        return Point(x + v.x, y+v.y, z+v.z);
        // printf("%lf %lf %lf\n", p.x, p.y, p.z);
-        return p;
 	}
     Vector operator-(Point p){
-        Vector v(x - p.x, y-p.y, z-p.z);
-        return v;
+        return Vector(x - p.x, y-p.y, z-p.z);
     }
 
      void set(double x, double y, double z){
@@ -155,10 +126,105 @@ public:
 
 };
 
+class Color{
+public:
+    int r;
+    int g;
+    int b;
+    Color(){
+        set(0,0,0);
+    }
+    Color(int r, int g, int b){
+        set(r, g, b);
+    }
+    void set(int r, int g, int b){
+        this->r = r;
+        this->g = g;
+        this->b = b;
+    }
+};
+
+class Triangle{
+public:
+    Point points[3];
+    Color color;
+};
 
 
+///============================Global Variables========================
+int screen_height, screen_width;
+double x_left_limit, x_right_limit, y_top_limit, y_bottom_limit;
+double z_front_limit, z_rear_limit;
+double dx, dy;
+double top_y, left_x;
+double** z_buffer;
+Color** frame_buffer;
+
+///==
+
+void read_data(){
+
+ifstream config("config.txt"), stage3("stage3.txt");
+config>>screen_width>>screen_height;
+config>>x_left_limit>>y_bottom_limit;
+
+config.close();
+stage3.close();
+}
+
+void pre_process(){
+    screen_width = 500;
+    screen_height = 300;
+}
+
+void initialize_z_buffer_and_frame_buffer(){
+
+    z_buffer = new double*[screen_width];
+    for(int i=0; i<screen_width; i++){
+        z_buffer[i] = new double[screen_height];
+    }
+    frame_buffer = new Color*[screen_width];
+    for(int i=0; i<screen_width; i++){
+        frame_buffer[i] = new Color[screen_height];
+    }
+}
+void pre_process2(){
+    for(int i=0; i<200; i++){
+        for(int j=0; j<100; j++){
+            frame_buffer[i][j].set(255,255,0);
+        }
+    }
+}
+
+void apply_procedure(){
+
+}
+
+void save(){
+    bitmap_image image(screen_width,screen_height);
+    for(int i=0;i<screen_width;i++){
+        for(int j=0;j<screen_height;j++){
+            image.set_pixel(i,j,frame_buffer[i][j].r,frame_buffer[i][j].g,frame_buffer[i][j].b);
+        }
+    }
+    image.save_image("1.bmp");
+
+    ///save z_buffer.txt
+}
+
+void free_memory(){
+
+}
 
 int main(){
-    cout<<"Hello world"<<endl;
+
+    read_data();
+    pre_process();
+    initialize_z_buffer_and_frame_buffer();
+    pre_process2();
+    apply_procedure();
+    save();
+    free_memory();
+
     return 0;
 }
